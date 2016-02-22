227b134b761b674a07d3c97c5b6a4a514703da35
Mike Verthein <mike.verthein@gmail.com>
2016-02-21 20:57:28 -0600

    Update to included other cohort info

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -5,7 +5,7 @@
 i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
 my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
 
-# Person 1's solution Mike V.
+# Person 1
 def my_array_finding_method(source, thing_to_find)
 new_array = []
 
@@ -41,17 +41,31 @@ def my_hash_finding_method(source, thing_to_find)
 
 # Person 2
 def my_array_modification_method!(source, thing_to_modify)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.map! { |x|
+    if x.is_a? Integer
+      x += thing_to_modify
+    else
+      x
+    end}
+  source
 end
 
 def my_hash_modification_method!(source, thing_to_modify)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.each do | pet, age |
+    source[pet] = (age + thing_to_modify)
+  end
+  source
 end
 
+p my_array_modification_method!(i_want_pets, 1)
+p my_hash_modification_method!(my_family_pets_ages, 2)
 # Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+# Map this allows you to identify all the elements in an array and and modify each seperately by iterating
+# is_a? is a boolean statement that allows you to see if an element meets a certain characteristic, in this case I used it to select only integers
+# In my second method I used the .each_do to modify each value in a hash
+
+
+#######Only 2 people in the group########
 
 
 # Person 3
@@ -106,9 +120,3 @@ end
 #
 
 
-# Release 3: Reflect!
-# What did you learn about researching and explaining your research to others?
-#
-#
-#
-#
