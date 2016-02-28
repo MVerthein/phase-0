8a1c4106f3db7841ca8c0e5d88c15b6135a76c50
Mike Verthein <mike.verthein@gmail.com>
2016-02-28 12:05:37 -0600

    Add solo and guided challenges

--- week-6/bingo_solution.rb
+++ week-6/bingo_solution.rb
@@ -1,30 +1,63 @@
 # A Nested Array to Model a Bingo Board SOLO CHALLENGE
 
-# I spent [#] hours on this challenge.
+# I spent [5] hours on this challenge.
+
+=begin
+Release 0: Pseudocode
+Outline:
+
+Create a method to generate a letter ( b, i, n, g, o) and a number (1-100)
+
+  Create an array with the letters b, i, n, g, o,
+  Create an array with random numbers 1-100 and change to string
+  Join the random letter, and the random number and print combination.
+
+Check the called column for the number called.
+
+ Method check, to iterate through each subarray in the board
+ IF the integer at the subarray and current hash value equals the current number
+
+If the number is in the column, replace with an 'x'
+
+  Set the number from check method to "x"
 
 
-# Release 0: Pseudocode
-# Outline:
+Display the board to the console (prettily)
 
-# Create a method to generate a letter ( b, i, n, g, o) and a number (1-100)
-  #fill in the outline here
-# => Create an array with the letters b, i, n, g, o,
-# => Create an array with random numbers 1-100 and change to string
-# =>
+  puts B I N G O for formatting, to identify column
+  print each subarray as a string, with each subarray on its
+    own line (hard return)
 
-# Check the called column for the number called.
-  #fill in the outline here
-# => If
-# If the number is in the column, replace with an 'x'
-  #fill in the outline here
-
-# Display a column to the console
-  #fill in the outline here
-
-# Display the board to the console (prettily)
-  #fill in the outline here
-
+=end
 # Initial Solution
+
+# class BingoBoard
+
+#   def initialize(board)
+#     @bingo_board = board
+#   end
+
+#   def pick
+#     @letters = {"B" => 0, "I" => 1, "N" => 2, "G" => 3, "O" =>  4}
+#     letter = @letters.keys.sample
+#     @numbers = rand(1..100).to_s
+#     @column = @letters[letter]
+#     p letter + @numbers
+#   end
+
+#   def check
+#     @bingo_board.each do |row|
+#       if row[@column] == @numbers.to_i
+#          row[@column] = "X"
+#       end
+#   end
+#     def display
+#      @bingo_board.map! {|x| p x.join(" ")}
+#     end
+#   end
+# end
+
+# Refactored Solution
 
 class BingoBoard
 
@@ -44,7 +77,7 @@ class BingoBoard
     match = false
     while !match do
       pick
-        sleep 0.1
+      sleep 0.2
     @bingo_board.each do |row|
       if row[@column] == @numbers.to_i
         match = true && row[@column] = "X"
@@ -58,10 +91,6 @@ class BingoBoard
   end
 end
 
-# Refactored Solution
-
-
-
 #DRIVER CODE (I.E. METHOD CALLS) GO BELOW THIS LINE
 board = [[47, 44, 71, 8, 88],
         [22, 69, 75, 65, 73],
@@ -70,7 +99,23 @@ board = [[47, 44, 71, 8, 88],
         [75, 70, 54, 80, 83]]
 
 new_game = BingoBoard.new(board)
-
+new_game.pick
 new_game.check
 new_game.display
 #Reflection
+=begin
+How difficult was pseudocoding this challenge? What do you think of your pseudocoding style?
+  - It was quite difficult for me to pseudocode on this challenge, I am still learning how to use proper pseudocoding technique. I think my style is to write out big picture ideas, and break it down from there.
+What are the benefits of using a class for this challenge?
+  - Using a class allows us to pass instance variables between methods.
+How can you access coordinates in a nested array?
+  - You can access items in a nested array with a doubled array calls array[][].
+What methods did you use to access and modify the array?
+  - I used #each to iterate through to access each array. Also used #while to loop through picks until there is a match.
+Give an example of a new method you learned while reviewing the Ruby docs. Based on what you see in the docs, what purpose does it serve, and how is it called?
+  - I used a new method of "while != do" so allow the pick method to loop until a match is hit, to then replace that with  "x".
+How did you determine what should be an instance variable versus a local variable?
+  - any variable that had to be used in the pick method and the check method was used as an instance variable.
+What do you feel is most improved in your refactored solution?
+  - Readabiltiy, and the way to format the displayed subarrays.
+=end
--- week-6/gps.rb
+++ week-6/gps.rb
@@ -1,42 +1,41 @@
 # Your Names
-# 1)
-# 2)
+# 1)Mike Verthein
+# 2)Brittney Braxton
 
-# We spent [#] hours on this challenge.
+# We spent [2] hours on this challenge.
 
 # Bakery Serving Size portion calculator.
 
 def serving_size_calc(item_to_make, num_of_ingredients)
   library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-  error_counter = 3
 
-  library.each do |food|
-    if library[food] != library[item_to_make]
-      error_counter += -1
-    end
-  end
+    raise ArgumentError.new("#{item_to_make} is not a valid input") unless library[item_to_make]
 
-  if error_counter > 0
-    raise ArgumentError.new("#{item_to_make} is not a valid input")
-  end
 
-  serving_size = library.values_at(item_to_make)[0]
+  serving_size = library[item_to_make]
   remaining_ingredients = num_of_ingredients % serving_size
 
-  case remaining_ingredients
-  when 0
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}"
+  if remaining_ingredients == 0
+      "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}"
   else
-    return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
+     "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Suggested baking items: #{remaining_ingredients} cookies!"
   end
 end
-
+p serving_size_calc("pie", 13)
 p serving_size_calc("pie", 7)
 p serving_size_calc("pie", 8)
 p serving_size_calc("cake", 5)
 p serving_size_calc("cake", 7)
 p serving_size_calc("cookie", 1)
 p serving_size_calc("cookie", 10)
-p serving_size_calc("THIS IS AN ERROR", 5)
+# p serving_size_calc("THIS IS AN ERROR", 5)
 
 #  Reflection
+# What did you learn about making code readable by working on this challenge?
+# => It is actually difficult making code readable when the code presented is not mine. While reading through the code presented we could make sense of the code, however ugly it was.
+# Did you learn any new methods? What did you learn about them?
+# => I personally did not learn any new methods, however did re-enforce methods previously learned.
+# What did you learn about accessing data in hashes?
+# => You can access data in a few different ways. If you want to access the key of the library in this example you can the key data using [].
+# What concepts were solidified when working through this challenge?
+# => Raising ArgumentError, and accessing hash data.
