30117245d2056150ea9a423d7a81440ce0f337d4
Mike Verthein <mike.verthein@gmail.com>
2016-03-17 15:08:17 -0500

    Add pair session 9.5

---
+++ week-9/fibonacci/fibonacci.rb
@@ -0,0 +1,31 @@
+# I worked on this challenge [by myself, with: ].
+# This challenge took me [#] hours.
+
+# Pseudocode
+
+
+
+# Initial Solution
+
+def is_fibonacci?(num)
+fn, f1, f2 = 0, 1, 1
+while fn != num
+    return false if fn > num
+    fn = f1 + f2
+    f2, f1 = f1, fn
+  end
+  true
+end
+
+
+
+
+# Refactored Solution
+
+
+
+
+
+
+
+# Reflection
